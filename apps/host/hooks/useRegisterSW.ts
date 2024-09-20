814ca10b2aa1e6585434f81351d2c44dc078b6f6
BM Loukas <loukas@brickmate.vn>
2024-09-20 10:58:29 +0700

    hotfix(sw):fix import file sw

--- apps/host/hooks/useRegisterSW.ts
+++ apps/host/hooks/useRegisterSW.ts
@@ -1,18 +1,18 @@
 import {useEffect} from "react";
 
 export const useRegisterSW = () => {
-    useEffect(() => {
-        if ('serviceWorker' in navigator) {
-            window.addEventListener('load', () => {
-                navigator.serviceWorker
-                    .register('/service-worker.js',)
-                    .then((registration) => {
-                        console.log('Service Worker registered: ', registration);
-                    })
-                    .catch((registrationError) => {
-                        console.log('Service Worker registration failed: ', registrationError);
-                    });
-            });
-        }
-    }, [])
+    // useEffect(() => {
+    //     if ('serviceWorker' in navigator) {
+    //         window.addEventListener('load', () => {
+    //             navigator.serviceWorker
+    //                 .register('/service-worker.js',)
+    //                 .then((registration) => {
+    //                     console.log('Service Worker registered: ', registration);
+    //                 })
+    //                 .catch((registrationError) => {
+    //                     console.log('Service Worker registration failed: ', registrationError);
+    //                 });
+    //         });
+    //     }
+    // }, [])
 }
